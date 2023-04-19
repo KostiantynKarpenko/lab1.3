f2941c3bf1a2baa5143b4487ad7bc9e8312d64b8
KostiantynKarpenko <10252215@stud.op.edu.ua>
2023-04-19 08:56:12 +0300

    modified:   css/index.css 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,15 @@
-
 const $btn = document.querySelector('.btn');
 
 const $popup = document.querySelector('.popup');
 
 const $overlay = document.querySelector('.overlay');
 
-$btn.addEventListener('click', function(event) {
-    $popup.classList.add('active')
-    $overlay.classList.add('visible')
-  });
+$btn.addEventListener('click', (e) => {
+    $popup.classList.add('active');
+    $overlay.classList.add('visible');
+});
 
-  document.addEventListener('click', function(event) {
-    if (!$popup.contains(event.target)) {
-        $popup.classList.remove('active')
-        $overlay.classList.remove('visible')
-    }
-  })
+$overlay.addEventListener('click', (e) => {
+    $popup.classList.remove('active');
+    $overlay.classList.remove('visible');
+});
